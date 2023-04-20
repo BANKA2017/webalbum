c80cd47e68c585cfa8639af6743e9e8203d32e0e
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2023-04-21 00:23:47 +0800

    update: about page by gpt-3.5

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class=" flex flex-col items-end my-10 text-sm">
-    <section class="text-gray-500 font-sans mb-1"><NuxtLink href="/about" class="font-bold underline underline-offset-2">Game Album</NuxtLink></section>
+    <section class="text-gray-500 font-sans mb-1"><NuxtLink href="/about" class="font-bold underline underline-offset-2">about</NuxtLink></section>
   </footer>
 </template>
 <script setup lang="ts">
--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="my-10">
     <div class="font-bold text-3xl mb-5">About Game <span class="underline underline-offset-2">Album</span></div>
-      <p class="text-lg">Game Album is an online photo album implemented with the <NuxtLink href="https://github.com/BANKA2017/twitter-monitor#cloudflare-workers" target="_blank" class="underline underline-offset-2">Twitter monitor online api</NuxtLink> for viewing or bulk downloading screenshots and videos stored on Twitter and shared from NS/PS/Xbox.</p>
+      <p class="text-lg">Game Album is an online album based on <NuxtLink href="https://github.com/BANKA2017/twitter-monitor#cloudflare-workers" target="_blank" class="underline underline-offset-2">Twitter monitor online api</NuxtLink>, used to view and download screenshots and videos sent to Twitter through NS/PS/Xbox sharing services.</p>
       <h3 class="text-2xl font-bold my-5"><span class="line-through">Unnecessary</span> Q&A</h3>
-      <p class="text-lg">Q: Why can't I share screen screenshots/videos to Twitter?</p>
-      <p class="text-lg">A: Unfortunately, I don't own any NS/PS/Xbox devices, so I can't answer these kinds of questions. In fact, the idea for this site came from a suggestion from a friend of mine.</p>
+      <p class="text-lg">Q: Can I view media that was not sent from NS/PS/Xbox?</p>
+      <p class="text-lg">A: Unfortunately, no. We can only view screenshots/videos shared from the NS/PS/Xbox sharing services.</p>
       <hr class="my-3" />
-      <p class="text-lg">Q: Can I view content shared from outside NS/PS/Xbox?</p>
-      <p class="text-lg">A: Unfortunately, No, we only support viewing content that has been shared from NS/PS/Xbox.</p>
+      <p class="text-lg">Q: Are you open source?</p>
+      <p class="text-lg">A: Yes, our backend is <NuxtLink href="https://github.com/BANKA2017/twitter-monitor/tree/node/apps/cfworkers" target="_blank" class="underline underline-offset-2">open source</NuxtLink> and the frontend will also be open sourced <span class="underline underline-offset-2 decoration-dotted">soon</span>.</p>
       <hr class="my-3" />
-      <p class="text-lg">Q: I saw a picture/video I posted with you, how do I ask you to remove it?</p>
-      <p class="text-lg">A: We do not have the power to manage users' Twitter accounts, so we cannot delete them. Users can delete the corresponding tweets from Twitter. Alternatively, if you don't want others to see your tweets, my advice is to link your NS/PS/Xbox account to a protected Twitter account, or simply not share them.</p>
+      <p class="text-lg">Q: I see the screenshots/videos I sent here, how do I request for you to delete them?</p>
+      <p class="text-lg">A: We are unable to delete this content as we do not have control over the tweets. In fact, anyone can easily find these contents through a simple search command (please check the components below). We suggest that you bind your NS/PS/Xbox to a protected Twitter account or simply refrain from sharing screenshots.</p>
       <hr class="my-3" />
-      <p class="text-lg">Q: I don't trust you, can I deploy myself?</p>
-      <p class="text-lg">A: It's a good idea - don't trust any third party websites, including us. The back end is <NuxtLink href="https://github.com/BANKA2017/twitter-monitor#cloudflare-workers" target="_blank" class="underline underline-offset-2">open source</NuxtLink> and the front end will be open source <span class="underline underline-offset-2 decoration-dotted">soon</span> - when we are ready. If you don't want to deploy it yourself, you can also paste the content below into Twitter's <NuxtLink :href="`https://twitter.com/search?q=${queryContent}&f=live`" class="underline underline-offset-2">search box</NuxtLink> and get the same result.</p>
+      <p class="text-lg">Q: Can I view these tweets directly on Twitter?</p>
+      <p class="text-lg">A: Yes, you can. Fill in the information below and <NuxtLink :href="`https://twitter.com/search?q=${queryContent}&f=live`" class="underline underline-offset-2">click here</NuxtLink>.</p>
       <div class="grid grid-cols-2 gap-5 mt-5">
           <div class="col-span-2 lg:col-span-1">
-              <input class="form-input rounded-xl w-full" placeholder="Twitter username like '@twitter'" v-model="state.screen_name">
+              <input class="form-input rounded-xl w-full" placeholder="Twitter username like @twitter" v-model="state.screen_name">
               <div class="grid grid-cols-3 mt-5 gap-2">
                   <div @click="state.platform = 'ns'" :class="{'col-span-3': true, 'lg:col-span-1': true, 'cursor-pointer': true, 'w-full': true, 'py-2': true, 'px-5': true, 'bg-[#E60012]': state.platform === 'ns', 'text-white': state.platform === 'ns', 'border-2': true, 'border-[#E60012]': state.platform !== 'ns',}">Nintendo Switch</div>
                   <div @click="state.platform = 'ps'" :class="{'col-span-3': true, 'lg:col-span-1': true, 'cursor-pointer': true, 'w-full': true, 'py-2': true, 'px-5': true, 'bg-[#0070D1]': state.platform === 'ps', 'text-white': state.platform === 'ps', 'border-2': true, 'border-[#0070D1]': state.platform !== 'ps',}">PlayStation</div>
@@ -32,7 +32,7 @@
       </ul>
 
       <div class=" flex flex-col items-end mt-5 mb-14 text-sm">
-          <p>Translated by DeepL, written by <NuxtLink href="https://nest.moe" class="font-bold hover:underline underline-offset-2">MANKA</NuxtLink> with ❤️</p>
+          <p>Translated by OpenAI gpt-3.5, written by <NuxtLink href="https://nest.moe" class="font-bold hover:underline underline-offset-2">MANKA</NuxtLink> with ❤️</p>
       </div>
   </div>
 </template>
